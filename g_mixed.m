8bf5bb4f7957bc9f4ca9ef2f991164fea5190efd
Alex Dombrovski <dombrovskia@upmc.edu>
2016-10-19 10:56:27 -0400

    Removed the expectancy lambda

--- g_mixed.m
+++ g_mixed.m
@@ -12,12 +12,12 @@ if cs || inG.noCS
 [gx(1),~,~] = sigm((Xt(1)*eta + u_t(2)*epsilon + lambda),[],beta); %% prediction of feedback ratings as a function of 
 ...expectancy (Xt) weighted by expectancy sensitivity eta + actual neurofeedback presented (u_t(2)) + bias lambda
     
-[gx(2),~,~] = sigm((Xt(1)+lambda),[],beta); %% prediction of infusion/no infusion expectancy ratings as a function of expectancy (Xt)
+[gx(2),~,~] = sigm((Xt(1)),[],beta); %% prediction of infusion/no infusion expectancy ratings as a function of expectancy (Xt)
 
 % exp_vigor = eta*Xt(1);
 else
 [gx(1),~,~] = sigm((Xt(2)*eta + u_t(2)*epsilon + lambda),[],beta);
-[gx(2),~,~] = sigm((Xt(2)+lambda),[],beta);
+[gx(2),~,~] = sigm((Xt(2)),[],beta);
 
 % exp_vigor = eta*Xt(2);
 end
